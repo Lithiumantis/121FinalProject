9409438d81093809f058ddffb8c59070516c8dc2
Lithiumantis <juyyek@ucsc.edu>
2019-02-24 20:09:05 -0800

    Copy project over from assignment 2

---
+++ Assets/PickerUpper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickerUpper : MonoBehaviour
+{
+    public Text scoreText;
+    private Movement movement;
+    private int score = 0;
+    private float thrust;
+
+    private AudioSource beepAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponentInParent<Movement>();
+        beepAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thrust = movement.thrust;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("pickup collision");
+        if (other.gameObject.tag == "Pickup")
+        {
+            score += (int)(thrust * 1000);
+            scoreText.text = score.ToString();
+            Destroy(other.gameObject);
+
+            beepAudio.Play();
+
+        }
+    }
+}
