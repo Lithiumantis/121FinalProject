c46394e6bc7d9f75b2467de630b824cda97f4656
Lithiumantis <juyyek@ucsc.edu>
2019-03-01 12:17:04 -0800

    Can switch between control types (advanced/standard)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,8 @@ public class Movement : MonoBehaviour
 {
     [System.NonSerialized]
     public bool paused;
+    public bool advancedControls = false;
+    public bool invertPitchControls = true;
 
     public float maxRoll = 90f;
     public float baseSpeed = 75f;
@@ -16,22 +18,15 @@ public class Movement : MonoBehaviour
     public float maxthrust = 5f;
     public float minthrust = 0.25f;
 
-
-    //makes controls more like a joystick
-    public bool invertPitchControls = true;
-
     //components
     public Text altitudeText;
     public Text thrustText;
-
 
     //vfx and sfx 
     protected Rigidbody rb;
     protected Vignette vignette;
     public PostProcessProfile postProcessProfile;
     protected AudioSource audioSource;
-
-
 
     //for getting mouse position on screen
     protected readonly float centerOfScreenX = Screen.width / 2;
@@ -53,6 +48,13 @@ public class Movement : MonoBehaviour
     }
 
     void FixedUpdate()
+    {
+        if (!advancedControls) StandardMovement();
+        else AdvancedMovement();
+
+    }
+
+    protected void StandardMovement()
     {
         //Debug.Log(GetMousePercentageX());
         if (!paused)
@@ -120,11 +122,7 @@ public class Movement : MonoBehaviour
             }
 
             //rotation
-            /*
-            rotateValue = new Vector3(0, x * -1.5f, z); //(0, yaw, roll)
-            transform.localEulerAngles = transform.localEulerAngles - rotateValue;
-            transform.Rotate(2*y, 0, 0, Space.Self); // pitch
-            */
+
             rotateValue = new Vector3(0, x * -1.5f, 0); //yaw
             transform.localEulerAngles = transform.localEulerAngles - rotateValue;
 
@@ -137,7 +135,92 @@ public class Movement : MonoBehaviour
 
             UpdateUI();
         }
+    }
 
+    protected void AdvancedMovement()
+    {
+        //Debug.Log(GetMousePercentageX());
+        if (!paused)
+        {
+            //movement variables
+            float x; float y; float z;
+
+
+            //mouse input
+            GetMousePosNormalized();
+            y = mouseY;
+            x = mouseX;
+
+            // GET ROLL
+            float manualRoll = 0.75f * Input.GetAxis("Roll");
+
+            //Case 1: Within acceptable zones for mouse movement
+            if (transform.eulerAngles.z < maxRoll || transform.eulerAngles.z > (360 - maxRoll))
+            {
+                z = 2 * Input.GetAxis("Mouse X") + manualRoll;
+            }
+            //Case 2: Too far to the right, but moving left
+            else if ((transform.eulerAngles.z > 180f && transform.eulerAngles.z < (360 - maxRoll)) && Input.GetAxis("Mouse X") <= 0f)
+            {
+                z = 2 * Input.GetAxis("Mouse X") + manualRoll;
+            }
+            //Case 3: Too far to the left, but moving right
+            else if ((transform.eulerAngles.z > maxRoll && transform.eulerAngles.z < 180f) && Input.GetAxis("Mouse X") >= 0f)
+            {
+                z = 2 * Input.GetAxis("Mouse X") + manualRoll;
+            }
+            else
+            {
+                z = 0 + manualRoll;
+            }
+
+            // GET THRUST, RESET TO REST POINT IF NO INPUT
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                thrust += Input.GetAxis("Vertical") * 0.07f;
+                if (thrust < minthrust) thrust = minthrust;
+                if (thrust > maxthrust) thrust = maxthrust;
+            }
+            else if (Input.GetAxis("Vertical") == 0 && thrust > thrustRestingPoint)
+            {
+                thrust -= 0.05f;
+                if (thrust < thrustRestingPoint)
+                {
+                    thrust = thrustRestingPoint;
+                }
+            }
+            else if (Input.GetAxis("Vertical") == 0 && thrust < thrustRestingPoint)
+            {
+                thrust += 0.05f;
+                if (thrust > thrustRestingPoint)
+                {
+                    thrust = thrustRestingPoint;
+                }
+            }
+
+
+            if (!invertPitchControls)
+            {
+                y = -y;
+            }
+
+            //rotation
+            
+            rotateValue = new Vector3(0, x * -1.5f, 2*z); //(0, yaw, roll)
+            transform.localEulerAngles = transform.localEulerAngles - rotateValue;
+            transform.Rotate(2*y, 0, 0, Space.Self); // pitch
+            /*
+            rotateValue = new Vector3(0, x * -1.5f, 0); //yaw
+            transform.localEulerAngles = transform.localEulerAngles - rotateValue;
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, maxRoll * GetMousePercentageX()); //roll
+            transform.Rotate(2 * y, 0, 0, Space.Self); // pitch
+            */
+
+            //forward thrust
+            rb.MovePosition(transform.position + transform.forward * baseSpeed * thrust * Time.deltaTime);
+
+            UpdateUI();
+        }
     }
 
     protected void GetMousePosNormalized()
@@ -190,6 +273,10 @@ public class Movement : MonoBehaviour
         {
             Debug.Log("Crash");
             vignette.intensity.value = 1;
+        }
+        else
+        {
+            Debug.Log("Hit");
         }
     }
 }
