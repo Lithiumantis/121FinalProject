1e65e84d599db2dd939e136ddcdc31b75d4b7fc6
Lithiumantis <juyyek@ucsc.edu>
2019-03-01 23:51:44 -0800

    more shooty

---
+++ Assets/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public ParticleSystem gunParticles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunParticles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            gunParticles.Play();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            gunParticles.Stop();
+        }
+    }
+
+
+}
